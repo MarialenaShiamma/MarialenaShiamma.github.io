fef6643dce24177ca08aff7888cd82e521bbe441
Marialena Shiamma <marialena93s@gmail.com>
2022-11-06 16:02:16 +0200

    needs improvent on events

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,22 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = document.getElementsByClassName("content")[i];
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel();
 });
@@ -150,18 +166,3 @@ $(".carousel-item-4").owlCarousel({
     },
   },
 })(jQuery);
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = document.getElementsByClassName("content")[i];
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
